0be7bcd6280b7e49450c47fd84de3e1380bbe436
Muhammad Ejaz Ur Rehman <muhammad.ejaz@systemsltd.com>
2020-09-28 13:38:39 +0500

    layout components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopBannerComponent } from './components/layout/top-banner/top-banner.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { MainSliderComponent } from './components/layout/main-slider/main-slider.component';
+import { BottomSliderComponent } from './components/layout/bottom-slider/bottom-slider.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBannerComponent,
+    NavbarComponent,
+    MainSliderComponent,
+    BottomSliderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/layout/bottom-slider/bottom-slider.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bottom-slider',
+  templateUrl: './bottom-slider.component.html',
+  styleUrls: ['./bottom-slider.component.css']
+})
+export class BottomSliderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
