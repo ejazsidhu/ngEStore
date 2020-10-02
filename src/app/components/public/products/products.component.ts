88e10ce19061f5b5daa9d57dbb2da637dfba95cb
Muhammad Ejaz Ur Rehman <muhammad.ejaz@systemsltd.com>
2020-10-02 20:40:18 +0500

    dynamic product liting added

--- src/app/components/public/products/products.component.ts
+++ src/app/components/public/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as data from "../products.json"
 
 @Component({
   selector: 'app-products',
@@ -12,59 +13,9 @@ export class ProductsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.productList = [
-      {
-        id: 1, title: 'carets',
-        description: `Integer tincidunt aliquet nibh vitae dictum. In turpis sapien, imperdiet quis magna nec,
-                          iaculis ultrices ante.Integer vitae suscipit nisi.Morbi dignissim risus sit amet orci porta,
-        eget aliquam purus
-                          sollicitudin.Cras eu metus felis.Sed arcu arcu, sagittis in blandit eu, imperdiet sit amet
-                          eros.Donec accumsan nisi purus, quis euismod ex volutpat in.Vestibulum eleifend eros ac
-                          lobortis aliquet.`, price: 10, imageUrl: 'assets/images/img-pro-01.jpg', tag: 'sale'
-      },
-      {
-        id: 2, title: 'tomato', description: `Integer tincidunt aliquet nibh vitae dictum. In turpis sapien, imperdiet quis magna nec,
-                          iaculis ultrices ante.Integer vitae suscipit nisi.Morbi dignissim risus sit amet orci porta,
-          eget aliquam purus
-                          sollicitudin.Cras eu metus felis.Sed arcu arcu, sagittis in blandit eu, imperdiet sit amet
-                          eros.Donec accumsan nisi purus, quis euismod ex volutpat in.Vestibulum eleifend eros ac
-                          lobortis aliquet.`, price: 50, imageUrl: 'assets/images/img-pro-02.jpg', tag: 'new'
-      },
-      {
-        id: 3, title: 'lasura', description: `Integer tincidunt aliquet nibh vitae dictum. In turpis sapien, imperdiet quis magna nec,
-                          iaculis ultrices ante.Integer vitae suscipit nisi.Morbi dignissim risus sit amet orci porta,
-            eget aliquam purus
-                          sollicitudin.Cras eu metus felis.Sed arcu arcu, sagittis in blandit eu, imperdiet sit amet
-                          eros.Donec accumsan nisi purus, quis euismod ex volutpat in.Vestibulum eleifend eros ac
-                          lobortis aliquet.`, price: 35, imageUrl: 'assets/images/img-pro-03.jpg', tag: 'sale'
-      },
-      {
-        id: 1, title: 'carets',
-        description: `Integer tincidunt aliquet nibh vitae dictum. In turpis sapien, imperdiet quis magna nec,
-                          iaculis ultrices ante.Integer vitae suscipit nisi.Morbi dignissim risus sit amet orci porta,
-        eget aliquam purus
-                          sollicitudin.Cras eu metus felis.Sed arcu arcu, sagittis in blandit eu, imperdiet sit amet
-                          eros.Donec accumsan nisi purus, quis euismod ex volutpat in.Vestibulum eleifend eros ac
-                          lobortis aliquet.`, price: 10, imageUrl: 'assets/images/img-pro-01.jpg', tag: 'sale'
-      },
-      {
-        id: 2, title: 'tomato', description: `Integer tincidunt aliquet nibh vitae dictum. In turpis sapien, imperdiet quis magna nec,
-                          iaculis ultrices ante.Integer vitae suscipit nisi.Morbi dignissim risus sit amet orci porta,
-          eget aliquam purus
-                          sollicitudin.Cras eu metus felis.Sed arcu arcu, sagittis in blandit eu, imperdiet sit amet
-                          eros.Donec accumsan nisi purus, quis euismod ex volutpat in.Vestibulum eleifend eros ac
-                          lobortis aliquet.`, price: 50, imageUrl: 'assets/images/img-pro-02.jpg', tag: 'new'
-      },
-      {
-        id: 3, title: 'lasura', description: `Integer tincidunt aliquet nibh vitae dictum. In turpis sapien, imperdiet quis magna nec,
-                          iaculis ultrices ante.Integer vitae suscipit nisi.Morbi dignissim risus sit amet orci porta,
-            eget aliquam purus
-                          sollicitudin.Cras eu metus felis.Sed arcu arcu, sagittis in blandit eu, imperdiet sit amet
-                          eros.Donec accumsan nisi purus, quis euismod ex volutpat in.Vestibulum eleifend eros ac
-                          lobortis aliquet.`, price: 35, imageUrl: 'assets/images/img-pro-03.jpg', tag: 'sale'
-      }
+    this.productList = (data as any).default;
+    console.log(this.productList)
 
-    ]
   }
 
 }
